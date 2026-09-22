49d6b4a3af70a78c72e2487abbbb172209114a89
DORON4649 <doron06yumayuma@gmail.com>
2026-09-22 15:33:22 +0900

    addtion a sanbai.java and a sosuu.java

---
+++ project3/src/main/java/com/example/day1/sanbai.java
@@ -0,0 +1,12 @@
+package com.example.day1;
+
+public class sanbai {
+    public static void main(String[] args) {
+    for(int i = 1; i <= 100; i++) {
+        if(i % 3 == 0) {
+            System.out.println(i);
+            }
+        
+         }
+    }
+}
